4420c53bf3fba2e12a0ef96f65050b2d035631e2
Ritvik Raj <ritvikraj71@gmail.com>
2025-05-07 00:04:07 -0400

    Update routing configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,15 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/ritvikraj-portfolio/' : '/',
+  base: '/ritvikraj-portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
   },
 })
